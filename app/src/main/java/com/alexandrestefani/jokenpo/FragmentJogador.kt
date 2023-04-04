e63f893c4374ec5cc40c3991136363851fd79011
Alexandre Marin Stefani <123953268+alexandrestefani@users.noreply.github.com>
2023-04-04 20:26:22 -0300

    Finalização do jogo
    Finalização do jogo

--- app/src/main/java/com/alexandrestefani/jokenpo/FragmentJogador.kt
+++ app/src/main/java/com/alexandrestefani/jokenpo/FragmentJogador.kt
@@ -1,9 +1,16 @@
 package com.alexandrestefani.jokenpo
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.navigation.Navigation
 import androidx.navigation.Navigation.findNavController
@@ -13,13 +20,20 @@ import com.alexandrestefani.jokenpo.databinding.FragmentjogadorBinding
 
 
 class FragmentJogador : Fragment() {
-    lateinit var root: View
+    private lateinit var root: View
+    private lateinit var selecionarJogada:  Spinner
+    private lateinit var onItemSelectedListener: OnItemSelectedListener
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        onItemSelectedListener = context as OnItemSelectedListener
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
+        Log.d("LifeCicle","onCreate_fragmentJogador")
     }
 
     override fun onCreateView(
@@ -28,10 +42,58 @@ class FragmentJogador : Fragment() {
     ): View? {
         val binding = FragmentjogadorBinding.inflate(inflater,container,false)
         root = binding.root
+        selecionarJogada = binding.spinner
+        setupSelecionarJogadasSpinner()
 
+
+
+        Log.d("LifeCicle","onCreateView_fragmentJogador")
         return root
     }
 
+    // metodos para seleção do spinner
+    private fun setupSelecionarJogadasSpinner(){
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.possibilidades_de_jogadas,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        selecionarJogada.adapter = adapter
+        selecionarJogada.onItemSelectedListener = onItemSelectedListener
+    }
+
+
+    // Life Cycle controle
+    override fun onStart() {
+        super.onStart()
+        Log.d("LifeCicle","onStart_fragmentJogador")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LifeCicle","onResume_fragmentJogador")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LifeCicle","onPause_fragmentJogador")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LifeCicle","onStop_fragmentJogador")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LifeCicle","onDestroy_fragmentJogador")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("LifeCicle","onDestroyView_fragmentJogador")
+    }
 
 
 
--- app/src/main/java/com/alexandrestefani/jokenpo/HosterActivity.kt
+++ app/src/main/java/com/alexandrestefani/jokenpo/HosterActivity.kt
@@ -2,10 +2,12 @@ package com.alexandrestefani.jokenpo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -16,13 +18,16 @@ import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.alexandrestefani.jokenpo.databinding.ActivityhosterBinding
 
-class HosterActivity : AppCompatActivity() {
+class HosterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
     lateinit var appBarConfiguration: AppBarConfiguration
     lateinit var navController: NavController
+    var jogadaAtual: String = "pedra"
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        Log.d("LifeCicle","onCreate")
 
         val binding = ActivityhosterBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -39,9 +44,31 @@ class HosterActivity : AppCompatActivity() {
 
         // configuração da DrawerNavigate - video em https://www.youtube.com/watch?v=TP4U3ejoC98 (1:10:18)
         binding.navidrawernew.setupWithNavController(navController)
+        binding.navidrawernew.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.fragmentoResultado -> {
+                    val args = Bundle()
+                    args.putString("jogadaAtual",jogadaAtual)
+                    navController.navigate(it.itemId,args)
+                }
+                else -> navController.navigate(it.itemId)
+            }
+            true
+        }
 
         // configuração bottonNavigation - video em https://www.youtube.com/watch?v=TP4U3ejoC98 (1:21:00)
         binding.bottonnavnew.setupWithNavController(navController)
+        binding.bottonnavnew.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.fragmentoResultado -> {
+                    val args = Bundle()
+                    args.putString("jogadaAtual",jogadaAtual)
+                    navController.navigate(it.itemId,args)
+                }
+                else -> navController.navigate(it.itemId)
+            }
+            true
+        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when(destination.id) {
@@ -58,5 +85,41 @@ class HosterActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration)  || super.onSupportNavigateUp()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LifeCicle","onStart")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LifeCicle","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LifeCicle","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LifeCicle","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LifeCicle","onDestroy")
+    }
+
+    //controla e guarda os itens de jogadas aqui ao inves do fragmento.
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val jogada = resources.getStringArray(R.array.possibilidades_de_jogadas)
+        jogadaAtual = jogada[position]
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
     }
 }
